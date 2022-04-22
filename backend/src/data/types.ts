bbecc7021831b61cd7ca5d8120dff667619df602
mo <arctic0809@gmail.com>
2022-04-22 17:46:38 +0100

    Added typing to request body

--- backend/src/data/types.ts
+++ backend/src/data/types.ts
@@ -1,8 +1,6 @@
-import * as core from 'express';
-import StoreData from './StoreData';
+import { Query } from 'express-serve-static-core';
 
-// export type Request<ReqBody = StoreData> = core.Request<ReqBody>;
-
-// export type Request = core.Request & StoreData;
-
-export type Request<ReqBody extends StoreData = any> = core.Request<ReqBody>;
+export type TypedRequest<T extends Query, U> = {
+    body: U;
+    query: T;
+} & Express.Request;
--- backend/src/routes/StoreRoute.ts
+++ backend/src/routes/StoreRoute.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import StoreData from '../data/StoreData';
 import { getStorageType, StorageType } from '../StorageType';
 import bodyParser from 'body-parser';
-import { Request } from '../data/types';
+import { TypedRequest } from '../data/types';
 
 export const storeRouter = express.Router();
 const jsonParser = bodyParser.json();
@@ -25,35 +25,42 @@ storeRouter.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send('Store Home');
 });
 
-storeRouter.get('/:key', (req: Request, res: express.Response) => {
-    const key: string = req.params.key;
-    const item = store.get(parseInt(key));
-    if (item !== undefined) {
-        res.status(200).send(item);
-    } else {
-        res.status(404).send('Item not found');
+storeRouter.get(
+    '/:key',
+    (req: TypedRequest<{ key: string }, StoreData>, res: express.Response) => {
+        const key: string = req.query.key;
+        const item = store.get(parseInt(key));
+        if (item !== undefined) {
+            res.status(200).send(item);
+        } else {
+            res.status(404).send('Item not found');
+        }
     }
-});
+);
 
-storeRouter.put('/:key', jsonParser, (req: Request, res: express.Response) => {
-    const item: StoreData = req.body;
-    item.id = parseInt(req.params.key);
-    console.log(item);
-    if (store.add(item)) {
-        res.status(200).send(item);
-    } else {
-        const obj = {
-            'Failed to add item': item,
-        };
-        res.status(400).send(obj);
+storeRouter.put(
+    '/:key',
+    jsonParser,
+    (req: TypedRequest<{ key: string }, StoreData>, res: express.Response) => {
+        const item: StoreData = req.body;
+        item.id = parseInt(req.query.key);
+        console.log(item);
+        if (store.add(item)) {
+            res.status(200).send(item);
+        } else {
+            const obj = {
+                'Failed to add item': item,
+            };
+            res.status(400).send(obj);
+        }
     }
-});
+);
 
 storeRouter.delete(
     '/deleteId/:key',
     jsonParser,
-    (req: Request, res: express.Response) => {
-        const key: string = req.params.key;
+    (req: TypedRequest<{ key: string }, StoreData>, res: express.Response) => {
+        const key: string = req.query.key;
         const deleted: boolean = store.removeById(parseInt(key));
         if (deleted === true) {
             res.status(200).send('Item deleted successfully');
@@ -66,8 +73,8 @@ storeRouter.delete(
 storeRouter.delete(
     '/deleteItemName/:name',
     jsonParser,
-    (req: Request, res: express.Response) => {
-        const key: string = req.params.name;
+    (req: TypedRequest<{ name: string }, StoreData>, res: express.Response) => {
+        const key: string = req.query.name;
         const deleted: boolean = store.removeByItemName(key);
         if (deleted === true) {
             res.status(200).send('Item deleted successfully');
